bc1b5fff938d412fddb6d79f546dfd0383cb9fec
oh1marin <sj2mail2@gmail.com>
2026-02-10 17:41:07 +0900

    이미지 기능 버킷 연결

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,8 @@ const nextConfig: NextConfig = {
       { protocol: "http", hostname: "localhost" },
       { protocol: "https", hostname: "api.marin-snack.store" },
       { protocol: "http", hostname: "api.marin-snack.store" },
+      { protocol: "https", hostname: "snack-bucket2.s3.ap-northeast-2.amazonaws.com" },
+      { protocol: "http", hostname: "snack-bucket2.s3.ap-northeast-2.amazonaws.com" },
     ],
   },
 };
